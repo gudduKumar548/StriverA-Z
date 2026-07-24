026fd8fccdef220e056f9c25512016ba35ed2fdc
gudduKumar548 <2237173.ece.cec@cgc.edu.in>
2026-07-24 23:55:34 +0530

    sort colors optimal approach

--- src/day7_Array_problems/Sort_colors.java
+++ src/day7_Array_problems/Sort_colors.java
@@ -5,18 +5,44 @@ import java.util.Scanner;
 
 public class Sort_colors {
     public void sortColors(int[] nums) {
-        int[] newnums = new int[3];
-        for(int i=0;i<nums.length;i++){
-            newnums[nums[i]]++;
-        }
-        int start=0;int count=0;
-        int n = newnums.length;
-        while(start<n){
-            for(int i=0;i<newnums[start];i++){
-                nums[count] = start;
-                count++;
+//  better approach
+
+//        int[] newnums = new int[3];
+//        for(int i=0;i<nums.length;i++){
+//            newnums[nums[i]]++;
+//        }
+//        int start=0;int count=0;
+//        int n = newnums.length;
+//        while(start<n){
+//            for(int i=0;i<newnums[start];i++){
+//                nums[count] = start;
+//                count++;
+//            }
+//            start++;
+//        }
+       // optimal approach (use dutch national flag algo )
+        // assume 0 to low-1 for all 0s, low to mid-1 for all 1s, mid to high-1 for unknown , high to end for all 2s
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
             }
-            start++;
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                int newtemp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = newtemp;
+                high--;
+            }
         }
     }
 }
